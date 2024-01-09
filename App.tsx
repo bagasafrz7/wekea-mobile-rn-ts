e3357a960c88ae78120330adc0050780d635c7b3
bagasafrz7 <bagasafrz16@gmail.com>
2024-01-09 19:00:32 +0700

    feat: add GestureHandlerRootView in wrapper code

--- App.tsx
+++ App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Routes from './src/routes'
 import CustomSafeArea from './src/components/CustomArea'
 import { PaperProvider } from 'react-native-paper'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const App = () => {
   return (
     <CustomSafeArea>
       <PaperProvider>
-        <Routes />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <Routes />
+        </GestureHandlerRootView>
       </PaperProvider>
     </CustomSafeArea>
   )
