b55b633a65dc869696da4b8a3069a9c1eb4bd9bd
bagasafrz7 <bagasafrz16@gmail.com>
2024-03-03 19:25:37 +0700

    feat: add helper priceFormating

---
+++ src/utils/helpers/priceFormating.ts
@@ -0,0 +1,6 @@
+export const priceFormating = (price: number) =>
+  new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    maximumFractionDigits: 0
+  }).format(price).replace('Rp', 'Rp. ')
